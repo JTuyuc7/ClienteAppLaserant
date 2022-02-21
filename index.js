cf145f1660867bd777b6ab90a5977220588a61cf
james tuyuc <jaimetuyuc7@gmail.com>
2022-02-21 12:24:47 -0600

    Frist Aplication
    Laserant Bootcamp first example

---
+++ index.js
@@ -0,0 +1,118 @@
+const form = document.getElementsByTagName('form')[0];
+/** @type {HTMLInputElement} */
+const inputCodigo = document.getElementById('codigo');
+/** @type {HTMLInputElement} */
+const inputNombre = document.getElementById('nombre');
+/** @type {HTMLInputElement} */
+const inputCantidad = document.getElementById('cantidad');
+/** @type {HTMLInputElement} */
+const inputPrecio = document.getElementById('precio');
+/** @type {HTMLInputElement} */
+const selectCategoria = document.getElementById('categoria');
+/** @type {HTMLInputElement} */
+const tbody = document.getElementsByTagName('tbody')[0];
+const cantidadTotalElement = document.getElementById('cantidad-total');
+const precioTotalElement = document.getElementById('precio-total');
+const granTotatlElement = document.getElementById('gran-total');
+
+let indice = 0;
+let cantidadTotal = 0;
+let precioTotales = 0;
+let granTotal = 0;
+let currentRow;
+
+form.addEventListener('submit', onSubmit);
+
+/**
+ * @param {Event} event
+ */
+
+function onSubmit(event){
+    event.preventDefault();
+    const data = new FormData(form);
+    const values = Array.from(data.entries);
+
+    const [frmCodigo, frmNombre, frmCantidad, frmPrecio, frmCategoria] = values;
+
+    let codigo = frmCodigo[1];
+    const nombre = frmNombre[1];
+    const cantidad = frmCantidad[1];
+    const precio = frmPrecio[1];
+    const categoria = frmCategoria[1];
+    const total = cantidad * precio;
+
+    cantidadTotal = parseFloat(cantidad);
+    precioTotales = parseFloat(precio);
+    granTotal = parseFloat(total) + granTotal;
+
+    let tr;
+
+    if(!codigo){
+        indice++;
+        codigo = indice;
+        tr = document.createElement('tr');
+        tbody.appendChild(tr);
+    }else {
+        tr = currentRow;
+    }
+
+    tr.dataset.categoria = categoria;
+    tr.innerHTML = `
+        <td>${codigo}</td>
+        <td>${nombre}</td>
+        <td>${cantidad}</td>
+        <td>${precio}</td>
+        <td>${total}</td>
+        <td> 
+            <div class="btn-group">
+                <a title="Editar" href="#" onclick"onEdit(event)" class="btn btn-sm btn-outline-secondary">
+                    <i class="bi bi-pencil-square"></i>
+                </a> 
+                <a title="Eliminar" href="#" onclick"onDelete(event)" class="btn btn-sm btn-outline-danger">
+                    <i class="bi bi-trash3"></i>
+                </a>
+            </div>
+        </td>
+    `;
+
+    cantidadTotalElement.innerText = cantidad;
+    precioTotalElement.innerText = precioTotales;
+    granTotatlElement.innerText = granTotal;
+    form.reset();
+}
+//https://www.facebook.com/681322435249967/posts/4815265908522245/?d=n
+
+/**
+ * @param {Event} event
+ */
+
+function onEdit(event){
+    event.preventDefault();
+
+    /** @type {HTMLElement} */
+    const anchor = event.currentTarget;
+    const tr = anchor.parentElement.parentElement.parentElement;
+    const celdas = tr.getElementsByTagName('td');
+    const [ tdCodigo, tdNombre, tdCantidad, tdPrecio ] = celdas;
+
+    inputCodigo.value = tdCodigo.innerText;
+    inputNombre.value = tdNombre.innerText;
+    inputCantidad.value = tdCantidad.innerText;
+    inputPrecio.value = tdPrecio.innerText;
+    selectCategoria.value = tr.dataset.categoria;
+
+    currentRow = tr;
+}
+
+/**
+ * @param {Event} event
+ */
+
+function onDelete(event){
+    event.preventDefault();
+    /**@type {HTMLElement} */
+
+    const anchor = event.currentTarget;
+    const tr = anchor.parentElement.parentElement.parentElement;
+    tbody.removeChild(tr)
+}
