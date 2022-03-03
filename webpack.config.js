9d51a08cc148103505d44dd4b5b6a0378ea0645e
james tuyuc <jaimetuyuc7@gmail.com>
2022-03-02 23:51:31 -0600

    Finalizando Configuracion webpack
    Configuraciones e instalaciones para manejar React

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin }= require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+const modeD = isDevelopment ? 'development' : 'production';
+const devPlugins = !isDevelopment ? [] : [ new ReactRefreshWebpackPlugin() ]
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, "dist"),
+        publicPath: ""
+    },
+    mode: modeD,
+    devServer: {
+        port: 5000,
+        open: true,
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                use: "babel-loader",
+                test: /.js$/,
+                exclude: /node_modules/
+            },
+            {
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+                test: /.css$/
+            },
+            {
+                type: "asset",
+                test: /.(png|svg|jpg|jpeg|gif)$/i
+            }
+        ]
+    },
+    plugins: [
+        //...devPlugins,
+        new ReactRefreshWebpackPlugin(),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css"
+        }),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        })
+    ]
+}
